a13f5c168dde70cfaa045da9a9756d622d4c7408
Gerf <73294402+GerfautGE@users.noreply.github.com>
2021-01-15 11:18:03 +0100

    documentation

--- fonctions.py
+++ fonctions.py
@@ -1,19 +1,30 @@
+"""
+Import des bibliothèques relatives au coprs F16
+"""
 import pyfinite.ffield as pffield
 import pyfinite.genericmatrix as pfmat
+
+"""
+Import de random pour gérer aléatoirement les erreurs
+"""
 from random import randint
 
-p = 4
-q = 2 ** p
-n = q - 1
-F = pffield.FField(p)
-a = 2
-k = 9
-t = (n - k) // 2
-l_0 = n - 1 - t
-l_1 = n - k - t
-nombreerreur=3
+"""
+Définition des paramètres du cadre d'étude :
+"""
+q = 2 ** 4 #nombre d'éléments de F16 => 16
+n = q - 1 #en python on compte à partir de 0
+F = pffield.FField(4) # F est le coprs F16
+a = 2 # élément primitif ie X
+k = 9 # taille du message envoyé choisi de sorte que l'on autorise 3 erreurs)
+nombreerreur = (n - k) // 2 #
+l_0 = n - 1 - nombreerreur #degré d'un polynome Q0
+l_1 = n - k - nombreerreur #degré d'un polynome Q1
 
 
+"""
+définition des fonctions utiles au calcul algébrique :
+"""
 def puissance(x, puiss):  # Définition de la puissance dans le corps F16
     c = 1
     for compteur in range(puiss):
@@ -29,7 +40,7 @@ def deg(polynome):  # retourn le degré du polynome
         # tous les coefficients sont nuls
 
 
-def simple(poly: list) -> list:
+def simple(poly: list) -> list: # retire les 0 inutiles en tête du polynome
     while len(poly) > 1:  # Tant que le polynome n'est pas une constante
         if poly[-1] == 0:
             poly.pop()  # On le retire le terme de plus haut degré s'il est nul
@@ -74,6 +85,10 @@ def divn(x, y):  # definition de la fonction division pour le calcul matriciel
         return F.Divide(x, y)
 
 
+"""
+Définition des éléments algébriques
+"""
+
 X = [0]  # Liste des Xi
 for i in range(2, n + 2):
     X.append(puissance(a, i - 2))
@@ -92,13 +107,38 @@ for i in range(n - k):
         ligne.append(puissance(X[j + 1], i + 1))
     H.SetRow(i, ligne)
 
+"""
+définition des fonctions qui opèrent sur les listes
+"""
 
+def erreur(n,L): # retourne L avec au plus n erreurs aléatoirement distribuées
+    for i in range(n):
+        position=randint(0,len(L)-1)
+        erreur=randint(0,16)
+        L[position]=erreur
+    return L
+
+def sont_egale(L,M): # test l'égalité de deux listes
+    if len(M) != len(L):
+        return False
+    else:
+        for i in range(len(L)):
+             if L[i] != M[i]:
+                 return False
+    return True
+
+"""
+Définition des fonctions de Reed-Solomon
+"""
 def encrypt(u):
     c = G.LeftMulColumnVec(u)
     return c
 
 
 def decrypt(message):
+    """
+    définition de M à partir du méssage reçu
+    """
     m = pfmat.GenericMatrix((n, l_0 + l_1 + 2), zeroElement=0, identityElement=1, add=adn, sub=adn, mul=multn, div=divn)
     for indice in range(1, n+1):
         ligne = []
@@ -108,9 +148,13 @@ def decrypt(message):
             ligne.append(F.Multiply(message[indice-1], puissance(X[indice], k)))
         m.SetRow(indice-1, ligne)
 
-    u = m.LUP()[1]
+    u = m.LUP()[1] # décomposition en matrices triangulaires pour extraire un élément non trivial du noyau
 
-    q = [X[1]]
+    """
+    récupération du noyau grâce au pivot de Gauss
+    """
+
+    q = [X[1]] # on fixe un élément pour obtenir un systeme de Cauchy
     taille = l_0 + l_1 + 2
     for i in range(1, taille):
         a = 0
@@ -122,40 +166,23 @@ def decrypt(message):
             a = F.Divide(a, u[taille - i - 1, taille - i - 1])
             q = [a] + q
 
-    q_1 = q[l_0 + 1:]
-    q_0 = q[:(len(q) - (l_1 + 1))]
-    return diveu(q_0, q_1)[0]
-
-def sont_egale(L,M): # test l'égalité de deux listes
-    if len(M) != len(L):
-        return False
-    else:
-        for i in range(len(L)):
-             if L[i] != M[i]:
-                 return False
-    return True
+    q_1 = q[l_0 + 1:] # définition de Q1
+    q_0 = q[:(len(q) - (l_1 + 1))] # définition de Q0
+    return diveu(q_0, q_1)[0] # le message initialement envoyé
 
 def encrypt_naif(L):
-    return L*(2*nombreerreur + 1) # principe des tiroirs
+    return L*3 # principe des tiroirs
 
 def decrypt_naif(L):
     n = len(L)
     D = []
-    c = [1]*(2*nombreerreur+1)
-    for i in range(2*nombreerreur+1):
-        D.append(L[k*i:k*(i+1)])
+    c = [1]*(3) # nombre d'éléments en commun entre les trois parties du message
+    for i in range(3):
+        D.append(L[k*i:k*(i+1)])# on découpe L en trois parties
     for i in range(len(D)-2):
         for j in range(i+1, len(D)-1):
-            if j<len(D) and sont_egale(D[i],D[j]):
+            if j<len(D) and sont_egale(D[i],D[j]):# on les compares deux à deux
                 D.pop(j)
                 c[i]+=c.pop(j)
     imax = c.index(max(c))
     return D[imax]
-
-
-def erreur(L):
-    for i in range(nombreerreur):
-        position=randint(0,len(L)-1)
-        erreur=randint(0,16)
-        L[position]=erreur
-    return L
