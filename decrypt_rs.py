bea364e1e707ece5fffaddeaf8812ee3bbae84bc
Gerf <73294402+GerfautGE@users.noreply.github.com>
2021-05-19 10:10:41 +0200

    commit

--- decrypt_rs.py
+++ decrypt_rs.py
@@ -6,13 +6,13 @@ def syndrome(message):
     s = H.LeftMulColumnVec(message)
     if sont_egale(s, [0]*(n-k)):
         return message
-    S = pfmat.GenericMatrix((l_1, l_1+1), zeroElement=0, identityElement=1, add=adn, sub=adn, mul=multn, div=divn)
+    S_ext = pfmat.GenericMatrix((l_1, l_1+1), zeroElement=0, identityElement=1, add=adn, sub=adn, mul=multn, div=divn)
     for i in range(l_1):
         ligne = []
         for j in range(l_1+1):
             ligne.append(s[i+j])
-        S.SetRow(i, ligne)
-        u = S.LUP()[1]  # décomposition en matrices triangulaires pour extraire un élément non trivial du noyau
+        S_ext.SetRow(i, ligne)
+        u = S_ext.LUP()[1]  # décomposition en matrices triangulaires pour extraire un élément non trivial du noyau
         """
         récupération du noyau grâce au pivot de Gauss
         """
@@ -28,14 +28,34 @@ def syndrome(message):
                 a = F.Divide(a, u[taille - i - 1, taille - i - 1])
                 b = [a] + b
     indice = []
+    b.reverse()
     for x in range(0,16):  # on test pour chaque élément du corps
         evaluation_q1 = 0
-        for w in range(l_1+1):  # pour chaque monome
-            evaluation_q1 = F.Add(evaluation_q1, F.Multiply(b[w], puissance(x, l_1-w)))
+        for l in range(l_1+1):  # pour chaque monome
+            e = evaluation_q1
+            evaluation_q1 = F.Add(e, F.Multiply(b[l], puissance(x, l_1-l)))
         if evaluation_q1 == 0:
-            indice.append(X.index(x))
-
-    return indice, b
+            indice.append(X.index(x)-1)
+    t = len(indice)
+    S_ext = pfmat.GenericMatrix((t, t), zeroElement=0, identityElement=1, add=adn, sub=adn, mul=multn, div=divn)
+    for i in range(t):
+        ligne = []
+        for j in range(t):
+            ligne.append(puissance(X[indice[j]], i+1))
+        S_ext.SetRow(i, ligne)
+        S_sol = [s[i] for i in range(t)]
+        I = S_sol
+        E = [0]*15
+        C = []
+        for i in range(16):
+            if i in indice:
+                E[i] = I[indice.index(i)]
+        for i in range(15):
+            if E[i] == 0:
+                C.append(message[i])
+            else:
+                C.append(F.Add(message[i], E[i]))
+    return S_ext,S_ext.Determinant()
 
 
 def polynomes(message):
--- test.py
+++ test.py
@@ -1,11 +1,9 @@
 from fonctions import *
 import decrypt_rs
+import time
 
 U = [i for i in range(9)]
 U = encrypt(U)
 V = erreur(U)
-print("U : ", U)
-print("V : ", V)
 print(decrypt_rs.syndrome(V))
-print("X :", X)
-#print(H)
+
