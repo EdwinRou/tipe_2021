ace86b63bac5c5142fa68ec37534b7b6d014a520
Gerf <73294402+GerfautGE@users.noreply.github.com>
2021-05-21 12:57:03 +0200

    comments

--- decode.py
+++ decode.py
@@ -2,54 +2,54 @@ from fonctions import *
 
 
 def syndrome(message):
-    assert (n-k) % 2 == 0  # MDS
+    assert (n-k) % 2 == 0  # Condition pour fonctionnement optimal des syndromes
     s, b = H.LeftMulColumnVec(message), []
-    if sont_egale(s, [0]*(n-k)):
+    if sont_egale(s, [0]*(n-k)):  # Si le Syndrome est nul (sans erreurs), on retourne le message
         return message
     s_ext = pfmat.GenericMatrix((l_1, l_1+1), zeroElement=0, identityElement=1, add=adn, sub=adn, mul=multn, div=divn)
-    for ligne_i in range(l_1):
+    for element in range(l_1):
         add_ligne = []
         for colonne_j in range(l_1+1):
-            add_ligne.append(s[ligne_i+colonne_j])
-        s_ext.SetRow(ligne_i, add_ligne)
+            add_ligne.append(s[element+colonne_j])
+        s_ext.SetRow(element, add_ligne)
         u = s_ext.LUP()[1]  # décomposition en matrices triangulaires pour extraire un élément non trivial du noyau
         """
         récupération du noyau grâce au pivot de Gauss
         """
         b = [X[1]]  # on fixe un élément pour obtenir un systeme de Cauchy
         taille = l_1 + 1
-        for ligne_i in range(1, taille):
+        for element in range(1, taille):
             a = 0
             for colonne_j in range(1, len(b) + 1):
-                a = F.Add(a, F.Multiply(u[taille - ligne_i - 1, taille - colonne_j], b[-colonne_j]))
+                a = F.Add(a, F.Multiply(u[taille - element - 1, taille - colonne_j], b[-colonne_j]))
             if a == 0:
                 b = [a] + b
             else:
-                a = F.Divide(a, u[taille - ligne_i - 1, taille - ligne_i - 1])
+                a = F.Divide(a, u[taille - element - 1, taille - element - 1])
                 b = [a] + b
     indice = []
     b.reverse()
-    for ligne_i in range(0, 16):  # on test pour chaque élément non nul du corps
+    for element in range(0, 16):  # on test pour chaque élément non nul du corps
         evaluation_q1 = 0
-        for l in range(l_1+1):  # pour chaque monome
+        for l in range(l_1+1):  # On somme chaque monome pour reformer Q1(element)
             e = evaluation_q1
-            evaluation_q1 = F.Add(e, F.Multiply(b[l], puissance(X[ligne_i], l_1-l)))
+            evaluation_q1 = F.Add(e, F.Multiply(b[l], puissance(X[element], l_1-l)))
         if evaluation_q1 == 0:
-            indice.append(ligne_i-1)
+            indice.append(element-1)
     t = len(indice)
-    H_ext = pfmat.GenericMatrix((t, t), zeroElement=0, identityElement=1, add=adn, sub=adn, mul=multn, div=divn)
+    h_ext = pfmat.GenericMatrix((t, t), zeroElement=0, identityElement=1, add=adn, sub=adn, mul=multn, div=divn)
     for m in range(t):
         add_ligne = []
         for l in range(t):
             add_ligne.append(puissance(X[indice[l]+1], m+1))
-        H_ext.SetRow(m, add_ligne)
-    S_sol = [s[i] for i in range(t)]
-    I = H_ext.Solve(S_sol)
+        h_ext.SetRow(m, add_ligne)
+    s_sol = [s[i] for i in range(t)]
+    I = h_ext.Solve(s_sol)
     E = [0]*15
-    for ligne_i in range(len(S_sol)):
-        E[indice[ligne_i]] = I[ligne_i]
-    C = [F.Add(message[i], E[i]) for i in range(15)]
-    return G_inverse.LeftMulColumnVec(C[:9])
+    for element in range(len(s_sol)):
+        E[indice[element]] = I[element]
+    C = [F.Add(message[i], E[i]) for i in range(9)]
+    return G_inverse.LeftMulColumnVec(C)
 
 
 def polynomes(message):
