2de6ec25445b6b069e4c3c103d40ff96f30d8e80
Gerf <73294402+GerfautGE@users.noreply.github.com>
2021-05-21 12:21:22 +0200

    commit

--- mesure_temps.py
+++ mesure_temps.py
@@ -9,7 +9,7 @@ def mesure_temps(n, method):
     X = [taille for taille in range(2, n, 10)]
     Y = []
     for taille in tqdm(range(2, n, 10)):
-        A = np.array([[[i + j, i + j, i + j] for j in range(taille)] for i in range(taille)])
+        A = np.array([[[(i + j)%255, (i + j)%255, (i + j)%255] for j in range(taille)] for i in range(taille)])
         t1 = time.time()
         modification_tableau_rs(erreur, A, taille, taille, method)
         t2 = time.time()-t1
@@ -18,10 +18,9 @@ def mesure_temps(n, method):
 
 
 X0, Y0 = mesure_temps(100, decode.polynomes)
-X, Y1 = mesure_temps(100, decode.syndrome)
-plt.plot(X0, Y0, label="R-S Polynômes")
-plt.plot(X0, Y1, label="R-S Syndromes")
-# plt.plot(X0, [i for i in X0], label="x")
+X0, Y1 = mesure_temps(100, decode.syndrome)
+Y2 = [Y0[i]/Y1[i] for i in range(len(Y1))]
+plt.plot(X0,Y2, label="Rapport des Complexités")
 plt.xlabel("Coté du tableau (Unités)")
 plt.ylabel("Temps d'execution (s)")
 plt.legend()
--- test.py
+++
@@ -1,9 +0,0 @@
-from fonctions import *
-import decrypt_rs
-import time
-
-U = [i for i in range(9)]
-U = encrypt(U)
-V = erreur(U)
-print(decrypt_rs.syndrome(V))
-
