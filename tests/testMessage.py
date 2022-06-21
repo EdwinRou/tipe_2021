7a5bcb485737ca7e7c001a955b712ba012981215
Gerf <73294402+GerfautGE@users.noreply.github.com>
2022-06-21 15:08:04 +0200

    commit

--- tests/testMessage.py
+++ tests/testMessage.py
@@ -1,13 +1,18 @@
 #coding:utf-8
 import sys, os
-sys.path.append(os.path.realpath(os.path.dirname('src')))
-from src.fonctions import k
+sys.path.append(os.path.realpath(os.path.dirname('src'))) # import src folder relative path
+from src.fonctions import k, G
+from src.decode import polynomes
 import unittest
 
 class TestMessage(unittest.TestCase):
     def test_is_message(self):
         m = [i for i in range(k)]
+        m = polynomes(G.LeftMulColumnVec(m))
         self.assertEqual(k, len(m))
+        for i in range(k):
+            self.assertEqual(i, m[i])
+        
 
 
 if __name__ == '__main__':
